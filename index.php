0f99a6b5d6330bfc2f66b468ca62d5cb0f56963d
root <root@raul.raul>
2017-12-02 12:32:46 +0100

    index add

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
 <!doctype html>
 <html lang="en">
   <head>
+
     <title>uabTheHack</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
     <h1>Hello, world!</h1>
+    <!-- Icons Grid -->
+    <section class="bg-light text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+              <button type="button" class="btn btn-success"><a href="controller/controller_groups.php" style="color:inherit">  SEE GROUP </a></button>
+              <h3></h3>
+              <p class="lead mb-0">Lorem ipsum dolor sit amet.</p>
+            </div>
+          <div class="col-lg-4">
+              <button type="button" class="btn btn-success">Success</button>
+              <h3>DOWNLOAD</h3>
+              <p class="lead mb-0">Lorem ipsum dolor sit amet.</p>
+            </div>
+          <div class="col-lg-4">
+              <button type="button" class="btn btn-success">Success</button>
+              <h3>Lorem ipsum</h3>
+              <p class="lead mb-0">Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
 
   </body>
 </html>
